542a69ff036974695b1291f62cef1f6a53ace62b
Jose Guaura <jguaura@8amarketing.com>
2020-10-14 17:00:35 -0300

    fixed babel build

--- src/database/db.js
+++ src/database/db.js
@@ -4,9 +4,9 @@ require('dotenv').config()
 //? Connect to remote db
 
 const sequelize = new Sequelize(
-    process.env.DB || 'd5334k36nomd4s',
-    process.env.USER || 'nqqupfbfsrwvgx',
-    process.env.PASS || 'bc8e372dfcf6aafdd4ac8808d033379f20597e07c4d3e61a6f2594875900dddc',
+    'd5334k36nomd4s',
+    'nqqupfbfsrwvgx',
+    'bc8e372dfcf6aafdd4ac8808d033379f20597e07c4d3e61a6f2594875900dddc',
     {
         host: process.env.HOST || 'ec2-75-101-232-85.compute-1.amazonaws.com',
         dialect: 'postgres',
