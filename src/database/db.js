c5e41b996cbad61d03cd02086553bdbdf7915372
Jose Guaura <jguaura@8amarketing.com>
2020-10-14 16:30:59 -0300

    test

--- src/database/db.js
+++ src/database/db.js
@@ -4,11 +4,11 @@ require('dotenv').config()
 //? Connect to remote db
 
 const sequelize = new Sequelize(
-    process.env.DB,
-    process.env.USER,
-    process.env.PASS,
+    process.env.DB || 'd5334k36nomd4s',
+    process.env.USER || 'nqqupfbfsrwvgx',
+    process.env.PASS || 'bc8e372dfcf6aafdd4ac8808d033379f20597e07c4d3e61a6f2594875900dddc',
     {
-        host: process.env.HOST,
+        host: process.env.HOST || 'ec2-75-101-232-85.compute-1.amazonaws.com',
         dialect: 'postgres',
         dialectOptions: {ssl: {rejectUnauthorized: false}},
         pool: {
